b4a04b85010b6ae1d44126f9aef52761df1592a5
bartw <The_Rebel666@hotmail.com>
2014-01-21 14:31:10 +0100

    Create Opponent.cs

---
+++ Models/Opponent.cs
@@ -0,0 +1,23 @@
+using System.Runtime.Serialization;
+
+namespace Teamr.Models
+{
+    [DataContract]
+    public class Opponent
+    {
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+        [DataMember(Name = "opponent_name")]
+        public string OpponentName { get; set; }
+        [DataMember(Name = "opponent_contact_phone")]
+        public string OpponentContactPhone { get; set; }
+        [DataMember(Name = "opponent_contact_email")]
+        public string OpponentContactEmail { get; set; }
+        [DataMember(Name = "opponent_contact_name")]
+        public string OpponentContactName { get; set; }
+        [DataMember(Name = "notes")]
+        public string Notes { get; set; }
+        [DataMember(Name = "league_controlled")]
+        public bool LeagueControlled { get; set; }
+    }
+}
