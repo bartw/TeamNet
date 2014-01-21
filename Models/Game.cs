a0f725959a5315cc49c2148b2178aad87998f6f1
bartw <The_Rebel666@hotmail.com>
2014-01-21 14:32:23 +0100

    Create Game.cs

---
+++ Models/Game.cs
@@ -0,0 +1,31 @@
+using System.Runtime.Serialization;
+
+namespace Teamr.Models
+{
+    [DataContract]
+    public class Game : Event
+    {
+        //"score_for" : null,
+        //"score_against" : null,
+        //"shootout_for" : null,
+        //"shootout_against" : null,
+        [DataMember(Name = "is_ot")]
+        public bool IsOt { get; set; }
+        [DataMember(Name = "is_shootout")]
+        public bool IsShootout { get; set; }
+        [DataMember(Name = "home_or_away")]
+        public int HomeOrAway { get; set; }
+        [DataMember(Name = "no_count")]
+        public bool NoCount { get; set; }
+        [DataMember(Name = "time_tbd")]
+        public bool TimeTbd { get; set; }
+        //"event_result" : null,
+        //"uniform" : null,
+        [DataMember(Name = "LeagueControlled")]
+        public bool LeagueControlled { get; set; }
+        [DataMember(Name = "opponent")]
+        public Opponent Opponent { get; set; }
+        //"formatted_result" : null,
+        //"availability_cutoff_time" : null
+    }
+}
