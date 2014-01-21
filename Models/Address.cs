c8ec3f24d3c564d6ee2f46226e6ff8241fc9c937
bartw <The_Rebel666@hotmail.com>
2014-01-21 14:33:52 +0100

    Create Address.cs

---
+++ Models/Address.cs
@@ -0,0 +1,21 @@
+using System.Runtime.Serialization;
+
+namespace Teamr.Models
+{
+    [DataContract]
+    public class Address
+    {
+        [DataMember(Name = "address")]
+        public string AddressLine1 { get; set; }
+        [DataMember(Name = "address2")]
+        public string AddressLine2 { get; set; }
+        [DataMember(Name = "city")]
+        public string City { get; set; }
+        [DataMember(Name = "State")]
+        public string State { get; set; }
+        [DataMember(Name = "zip")]
+        public string Zip { get; set; }
+        [DataMember(Name = "country")]
+        public string Country { get; set; }
+    }
+}
